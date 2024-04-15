13d4edbf9183b893e4281aae0a710ebccf7c0342
Flex58 <164533598+Flex58@users.noreply.github.com>
2024-04-15 12:20:19 +0200

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if ((typeof num1 !== "number" || typeof num2 !== "number") || (num1 < 0 || num2 < 0)){
+        return "ERROR";
+    }
 
+    else if (num1 < num2){
+        while (num1 <= num2){
+            sum += num1;
+            num1++;
+        }
+        return sum;
+    }
+
+    else if (num1 > num2){
+        while (num1 >= num2){
+            sum += num1;
+            num1--;
+        }
+        return sum;
+    }
+    
 };
 
 // Do not edit below this line
