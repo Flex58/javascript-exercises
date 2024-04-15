98174359fdff1137611ead4ab09ce607c66fc020
Flex58 <164533598+Flex58@users.noreply.github.com>
2024-04-15 13:15:51 +0200

    Finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let result = (num - 32) * (5/9);
+  let rounded = Math.round(result * 10)/10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let result = num * (9/5) + 32;
+  let rounded = Math.round(result * 10)/10;
+  return rounded;
 };
 
 // Do not edit below this line
