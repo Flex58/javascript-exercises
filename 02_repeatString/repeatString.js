cf785cd15ae17149ef11d9d76dd6d414e27ed222
Flex58 <164533598+Flex58@users.noreply.github.com>
2024-04-15 10:04:55 +0200

    Finish exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+let j = '';
+if (num < 0){
+    return 'ERROR';
+}
+for (let i = 0; i < num; i++){
+    j += string;
+}
+return j;
 };
 
 // Do not edit below this line
