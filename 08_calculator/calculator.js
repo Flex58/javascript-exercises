ea031c3bdac857beacaefde842415ac182b13bc2
Flex58 <164533598+Flex58@users.noreply.github.com>
2024-05-28 10:58:08 +0200

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+ let result = arr.reduce((collector, value) => collector + value, 0)
+ return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = arr.reduce((collector, value) => collector * value)
+ return result;
 };
 
-const power = function() {
-	
+const power = function(x, n) {
+  let y = x;
+	for (let i = 1; i < n; i++)
+  {
+    x = x * y;
+  }
+  return x;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  for (let i = x; i != 0; i--)
+  {
+    if (i > 1){
+      x *= (i - 1)
+      console.log(x)
+    }
+    else
+    {
+      x *= i;
+    }
+  }
+  if ( x == 0){
+    return 1
+  }
+  else {
+	  return x;
+  }
 };
 
 // Do not edit below this line
