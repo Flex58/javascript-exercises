464a2452e7674eca3357aa883a6a3d62196413a2
Flex58 <164533598+Flex58@users.noreply.github.com>
2024-05-29 11:41:21 +0200

    finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let fibonacci = [1,1];
+    let reduced;
+    if (n == 0){
+        return 0;
+    }
+    else if (n < 0){
+        return "OOPS"
+    }
+    else if (n == 1 || n == 2){
+        return 1;
+    }
+    for (let i = 0; i < n -2; i++){
+        reduced = fibonacci.reduce((sum, current) => sum + current, 0)
+        fibonacci.shift()
+        fibonacci.push(reduced)
+    }
+    return reduced;
 };
 
 // Do not edit below this line
