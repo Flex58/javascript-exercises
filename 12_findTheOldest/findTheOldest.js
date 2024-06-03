f13915d03a67f739f150d1c027859b79a7b73c2c
Flex58 <164533598+Flex58@users.noreply.github.com>
2024-06-03 10:43:44 +0200

    finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const date = new Date().getFullYear();
+    const sorted = arr.sort((a,b) => {
+        if (a.yearOfDeath == undefined){
+            return date - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth ? 1:-1
+        }
+        else if (b.yearOfDeath == undefined){
+            return a.yearOfDeath - a.yearOfBirth < date - b.yearOfBirth ? 1:-1
+        }
+        return a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth ? 1:-1
+    })
+    return sorted[0]
 };
 
 // Do not edit below this line
